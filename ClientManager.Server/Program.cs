7adae596e6c6cbc886479197eedfa137431faa2a
Serhiy <toroniyserhiy@gmail.com>
2024-03-10 12:41:42 +0200

    Add project files.

---
+++ ClientManager.Server/Program.cs
@@ -0,0 +1,38 @@
+using ClientManager.Server.Models;
+using Microsoft.EntityFrameworkCore;
+using MySqlConnector;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddMySqlDataSource(builder.Configuration.GetConnectionString("Default")!);
+builder.Services.AddDbContext<DatabaseContext>(options =>
+    options.UseMySQL(builder.Configuration.GetConnectionString("DbConnection")!));
+
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapFallbackToFile("/index.html");
+
+app.Run();
